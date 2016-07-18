09032b7a6b99759de4d92a668f1ff2fcfea5c24a
acastelain <alexandre.castelain@orexad.com>
2016-07-18 14:19:27 +0200

    [BASE] Left and right menus are managed by a controller + a controller manages the page "documents références"

--- src/FrontBundle/Controller/MainController.php
+++ src/FrontBundle/Controller/MainController.php
@@ -6,8 +6,47 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class MainController extends Controller
 {
-    public function indexAction()
+    public function newsAction()
     {
-        return $this->render('FrontBundle:Main:index.html.twig');
+        return $this->render('FrontBundle:Main:news.html.twig');
+    }
+
+    public function documentsAction() {
+        return $this->render("FrontBundle:Main:documents.html.twig");
+    }
+
+    public function sideMenuAction($position) {
+        $menus = array();
+
+        if ($position == "left") {
+            $element["href"] = "#";
+            $element["alt"] = "Catalogue Usinage";
+            $element["src"] = "bundles/front/img/img-usinage_100.png";
+            array_push($menus, $element);
+
+            $element["href"] = "#";
+            $element["alt"] = "Catalogue transmission";
+            $element["src"] = "bundles/front/img/img-transmission_100.png";
+            array_push($menus, $element);
+        }
+        else if ($position == "right") {
+            $element["href"] = "#";
+            $element["alt"] = "Arrivages Orexad n°2";
+            $element["src"] = "bundles/front/img/pack2.png";
+            array_push($menus, $element);
+
+            $element["href"] = "#";
+            $element["alt"] = "Bons plans 24";
+            $element["src"] = "bundles/front/img/bp24.jpg";
+            array_push($menus, $element);
+
+            $element["href"] = "#";
+            $element["alt"] = "Catalogue GISS";
+            $element["src"] = "bundles/front/img/img_giss_catalogue.jpg";
+            array_push($menus, $element);
+        }
+
+
+        return $this->render("FrontBundle:Main:sideMenu.html.twig", array("menus" => $menus));
     }
 }
