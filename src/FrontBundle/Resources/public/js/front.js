09298be27760990df083a5a687751e7cb63a803a
acastelain <alexandre.castelain@orexad.com>
2016-07-19 08:58:55 +0200

    [BASE] The template for the news is created

--- src/FrontBundle/Resources/public/js/front.js
+++ src/FrontBundle/Resources/public/js/front.js
@@ -5,3 +5,8 @@ $(".nav a").on("click", function(){
     $(".nav").find(".active").removeClass("active");
     $(this).parent().addClass("active");
 });
+
+$(".list-group.small li").on("click", function() {
+    $(".list-group.small").find(".active").removeClass("active");
+    $(this).addClass("active");
+});
