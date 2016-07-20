c2a0be9e01a8dc510294572e85913b435e487554
acastelain <alexandre.castelain@orexad.com>
2016-07-20 15:52:29 +0200

    [BASE] The datatable is responsive + simple correction for a good responsive display + change the name of a route  (add a 's')

--- src/FrontBundle/Resources/public/js/front.js
+++ src/FrontBundle/Resources/public/js/front.js
@@ -13,14 +13,17 @@ $(".list-group.small a").on("click", function() {
 
 $(document).ready(function() {
     $('#table-document-downloadable').DataTable( {
+        responsive: true,
+        autoWidth: false,
         language: {
             "sProcessing":     "Traitement en cours...",
-            "sSearch":         "Rechercher&nbsp;:",
+            "sSearch":         "",
             "sLengthMenu":     "Afficher _MENU_ &eacute;l&eacute;ments",
-            "sInfo":           "Affichage de l'&eacute;l&eacute;ment _START_ &agrave; _END_ sur _TOTAL_ &eacute;l&eacute;ments",
+            "sInfo":           "Affichage de l'&eacute;l&eacute;ment _START_ &agrave; _END_ sur _TOTAL_",
             "sInfoEmpty":      "Affichage de l'&eacute;l&eacute;ment 0 &agrave; 0 sur 0 &eacute;l&eacute;ment",
             "sInfoFiltered":   "(filtr&eacute; de _MAX_ &eacute;l&eacute;ments au total)",
             "sInfoPostFix":    "",
+            "searchPlaceholder": "Recherche",
             "sLoadingRecords": "Chargement en cours...",
             "sZeroRecords":    "Aucun &eacute;l&eacute;ment &agrave; afficher",
             "sEmptyTable":     "Aucune donn&eacute;e disponible dans le tableau",
