c05481fefb823445a5fc19c189c67971122834c5
acastelain <castelain.alexandre@outlook.com>
2017-01-09 15:36:39 +0100

    [FRONT][MAIL] We can reset our password when we forget it once every 24h

--- src/Front/AppBundle/Controller/HelpController.php
+++ src/Front/AppBundle/Controller/HelpController.php
@@ -14,20 +14,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class HelpController extends Controller
 {
     public function indexAction() {
-        /** @var \Swift_Mime_Message $message */
-        $message = \Swift_Message::newInstance()
-            ->setSubject('Hello Email')
-            ->setFrom(array("alexandre.castelain@orexad.com" => "Je suis un genie"))
-            ->setTo('calex92@gmail.com')
-            ->setBody(
-                $this->renderView(
-                    'mail/test.html.twig'
-                ),
-                'text/html'
-            )
-            ->setCharset('utf-8')
-        ;
-        $this->get('mailer')->send($message);
         return $this->render("FrontAppBundle:Help:index.html.twig");
     }
 }
