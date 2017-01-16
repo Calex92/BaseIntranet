30104597e9f75f54e30b648d8499b76c255ae4cc
acastelain <castelain.alexandre@outlook.com>
2017-01-16 14:24:22 +0100

    [FRONT] [GROUP] When we update a group, the prefered profiles are removed to prevent some bug if the user got a prefered profile not real

--- src/Admin/AppBundle/Controller/GroupController.php
+++ src/Admin/AppBundle/Controller/GroupController.php
@@ -12,6 +12,7 @@ namespace Admin\AppBundle\Controller;
 use Admin\AppBundle\Enum\RightsEnum;
 use Admin\AppBundle\Form\GroupType;
 use Front\AppBundle\Entity\Group;
+use Front\UserBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -45,14 +46,23 @@ class GroupController extends Controller
         ));
     }
 
-    public function updateAction(Request $request, $id) {
-        $group = $this->get("doctrine")->getRepository("FrontAppBundle:Group")->find($id);
+    public function updateAction(Request $request, Group $group) {
         $form = $this->get("form.factory")->create(GroupType::class, $group);
 
         if ($request->isMethod("POST") && $form->handleRequest($request)->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
+            $em = $this->getDoctrine()->getManager();
 
-            $this->addFlash("success", "Le groupe a bien été modifié");
+            // We have to remove the prefered profile for each users from this group.
+            // We should also create a notification to explain why the profiles are removed.
+            foreach ($group->getUsers() as $user) {
+                /** @var User $user */
+                foreach ($user->getProfilesPrefered() as $preferedProfile) {
+                    $em->remove($preferedProfile);
+                }
+            }
+
+            $em->flush();
+            $this->addFlash("success", "Le groupe a bien été modifié, les profils préférés des utilisateurs ont été réinitialisés.");
             return $this->redirectToRoute("admin_group_manager_homepage");
         }
         return $this->render("AdminAppBundle:Group:update.html.twig", array(
