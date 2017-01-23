ed98fbc593950703e0eff6037e1be309b89be910
acastelain <castelain.alexandre@outlook.com>
2017-01-23 17:40:42 +0100

    [GENERAL] Save before Symfony Update

--- src/Front/AppBundle/Controller/ApplicationsController.php
+++ src/Front/AppBundle/Controller/ApplicationsController.php
@@ -29,7 +29,7 @@ class ApplicationsController extends Controller
     }
 
     /**
-     * @Security("has_role('IS_AUTHENTICATED_REMEMBERED')
+     * @Security("has_role('IS_AUTHENTICATED_REMEMBERED')")
      * @param $applicationId
      * @return \Symfony\Component\HttpFoundation\RedirectResponse
      */
